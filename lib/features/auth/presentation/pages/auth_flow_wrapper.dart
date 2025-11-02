e1751895a7701bdb0369fb44d8b1cb88298a2589
SuperSteban <jestebansantti@gmail.com>
2025-11-02 14:22:22 -0700

    feat: agrega navbar funcional con pantallas hacia productos, usuario y settings, arregla splash screen

--- lib/features/auth/presentation/pages/auth_flow_wrapper.dart
+++ lib/features/auth/presentation/pages/auth_flow_wrapper.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import '../pages/sign_in_screen.dart';
 import '../provider/sign_in_provider.dart';
-
-// Importamos el estado para poder usarlo en la escucha (aunque no es estrictamente necesario aquí)
+import 'home_screen.dart';
 
 
 class AuthFlowWrapper extends ConsumerWidget {
@@ -13,9 +12,6 @@ class AuthFlowWrapper extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     // 1. Observar el estado actual
     final authState = ref.watch(authNotifierProvider);
-
-    // 2. Leer el Notifier para llamar a los métodos
-    final notifier = ref.read(authNotifierProvider.notifier);
 
     // --- Lógica de Decisión ---
 
@@ -27,45 +23,11 @@ class AuthFlowWrapper extends ConsumerWidget {
     // 4. Estado de Éxito (Autenticado)
     // Evaluamos si la propiedad 'user' NO es null
     if (authState.user != null) {
-      final user = authState.user!; // Obtenemos la entidad garantizada
-
-      return Scaffold(
-        appBar: AppBar(
-          title: const Text('Home / Inventario'),
-          actions: [
-            TextButton(
-              onPressed: authState.isLoading ? null : () {
-                // Llama al método signOut del AuthNotifier
-                notifier.signOut();
-              },
-              child: authState.isLoading
-                  ? const SizedBox(
-                width: 20,
-                height: 20,
-                child: CircularProgressIndicator(color: Colors.white, strokeWidth: 2),
-              )
-                  : const Text('Cerrar Sesión', style: TextStyle(color: Colors.white)),
-            ),
-          ],
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Autenticado. Bienvenido, ${user.email}!', style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-              const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Text('Esta es la pantalla principal del Inventario (HomeScreen).'),
-              ),
-            ],
-          ),
-        ),
-      );
+      return const HomeScreen();
     }
 
     // 5. Estado Inicial / Error / Desconectado
     // Si no está cargando y 'user' es null, mostramos la pantalla de inicio de sesión.
-    // 🟢 ESTO ES LA REDIRECCIÓN AUTOMÁTICA
     return const SignInScreen();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 // Importación de la función de configuración de GetIt
@@ -12,17 +13,27 @@ import 'features/auth/presentation/pages/auth_flow_wrapper.dart';
 
 void main() async {
   // 1. Inicialización de Flutter
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding =  WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: (widgetsBinding));
+  await _initializeApp();
+  // 4. Ejecutar la aplicación
+  runApp(const ProviderScope(child: MyApp()));
+}
 
-  // 2. Inicialización de Firebase
+// Función separada para manejar toda la lógica de inicialización.
+Future<void> _initializeApp() async {
+  // 1. Inicialización de Firebase
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
-  // 3. Inicialización de la Inyección de Dependencias (Get_it)
+  // 2. Inicialización de la Inyección de Dependencias (Get_it)
   // CRÍTICO: Esta función registra todos los Repositorios y Use Cases.
   setupDependencies();
 
-  // 4. Ejecutar la aplicación
-  runApp(const ProviderScope(child: MyApp()));
+  // 3. Simulamos una pequeña espera si es necesario, o cargamos datos iniciales.
+  // Quítalo si no lo necesitas.
+  await Future.delayed(const Duration(seconds: 1));
+
+  // 4. ¡CRÍTICO! Eliminar la pantalla nativa de splash
+  FlutterNativeSplash.remove();
 }
 
 class MyApp extends StatelessWidget {
